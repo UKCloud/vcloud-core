ec37faa6862b06d4e490365e9648f31dd9423707
Bobby DeVeaux <me@bobbyjason.co.uk>
2017-07-03 14:35:06 +0100

    adding env vars

--- scripts/setfog.sh
+++ scripts/setfog.sh
@@ -3,5 +3,5 @@ set -ev
 
 echo "test_credential:" > ~/.fog
 echo "	vcloud_director_host: 'api.vcd.pod0000b.sys00005.portal.skyscapecloud.com/api'" >> ~/.fog
-echo "	vcloud_director_username: 'meh'" >> ~/.fog
-echo "	vcloud_director_password: ''" >> ~/.fog
+echo "	vcloud_director_username: '$FOG_USERNAME'" >> ~/.fog
+echo "	vcloud_director_password: '$FOG_PASSWORD'" >> ~/.fog
