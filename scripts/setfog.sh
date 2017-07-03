d92bf398a25566731c80cd83ff9e7eb21054b97c
Bobby DeVeaux <me@bobbyjason.co.uk>
2017-07-03 14:39:48 +0100

    replacing tabs for spaces

--- scripts/setfog.sh
+++ scripts/setfog.sh
@@ -2,6 +2,6 @@
 set -ev
 
 echo "test_credential:" > ~/.fog
-echo "	vcloud_director_host: 'api.vcd.pod0000b.sys00005.portal.skyscapecloud.com/api'" >> ~/.fog
-echo "	vcloud_director_username: '$FOG_USERNAME'" >> ~/.fog
-echo "	vcloud_director_password: '$FOG_PASSWORD'" >> ~/.fog
+echo "    vcloud_director_host: 'api.vcd.pod0000b.sys00005.portal.skyscapecloud.com/api'" >> ~/.fog
+echo "    vcloud_director_username: '$FOG_USERNAME'" >> ~/.fog
+echo "    vcloud_director_password: '$FOG_PASSWORD'" >> ~/.fog
