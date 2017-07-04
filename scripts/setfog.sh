41fc2a54efc5a2ca019b64cf251971fdc31a2ef4
Bobby DeVeaux <me@bobbyjason.co.uk>
2017-07-04 09:06:54 +0100

    adding vcloud-login

--- scripts/setfog.sh
+++ scripts/setfog.sh
@@ -4,4 +4,6 @@ set -ev
 echo "test_credential:" > ~/.fog
 echo "    vcloud_director_host: 'api.vcd.pod0000b.sys00005.portal.skyscapecloud.com/api'" >> ~/.fog
 echo "    vcloud_director_username: '$FOG_USERNAME'" >> ~/.fog
-echo "    vcloud_director_password: '$FOG_PASSWORD'" >> ~/.fog
+echo "    vcloud_director_password: ''" >> ~/.fog
+
+eval `echo $FOG_PASSWORD |  FOG_CREDENTIAL=test_credential vcloud-login`
