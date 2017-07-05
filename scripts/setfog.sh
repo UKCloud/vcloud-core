4b900d44a3a94c6e2b4e3990ba31236923ee5ef5
Bobby DeVeaux <me@bobbyjason.co.uk>
2017-07-05 12:00:41 +0100

    adding vars

--- scripts/setfog.sh
+++ scripts/setfog.sh
@@ -12,10 +12,12 @@ echo "    catalog: 'Skyscape Catalogue'" >> ./spec/integration/vcloud_tools_test
 echo "    vapp_template: 'Skyscape_CentOS_6_4_x64_50GB_Small_v1.0.1'" >> ./spec/integration/vcloud_tools_testing_config.yaml
 echo "    default_storage_profile_name: 'bobbywat'" >> ./spec/integration/vcloud_tools_testing_config.yaml
 echo "    network_1: 'bobby_network1'" >> ./spec/integration/vcloud_tools_testing_config.yaml
+echo "    network_1_id: ''" >> ./spec/integration/vcloud_tools_testing_config.yaml
 echo "    network_1_ip: '10.10.0.1'" >> ./spec/integration/vcloud_tools_testing_config.yaml
 echo "    network_2: 'bobby_network2'" >> ./spec/integration/vcloud_tools_testing_config.yaml
+echo "    network_2_id: ''" >> ./spec/integration/vcloud_tools_testing_config.yaml
 echo "    network_2_ip: '10.20.0.1'" >> ./spec/integration/vcloud_tools_testing_config.yaml
 echo "    storage_profile: 'wat'" >> ./spec/integration/vcloud_tools_testing_config.yaml
 echo "    edge_gateway: 'Internet_04(nti0000bi2_467-735-2)'" >> ./spec/integration/vcloud_tools_testing_config.yaml
+echo "    edge_gateway_id: 'Internet_04(nti0000bi2_467-735-2)'" >> ./spec/integration/vcloud_tools_testing_config.yaml
 
-eval "$(echo $FOG_PASSWORD |  FOG_CREDENTIAL=test_credential vcloud-login)"
